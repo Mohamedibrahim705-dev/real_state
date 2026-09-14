e1a57e12d53171deff970dccaa9754e4885cc05e
Mohamedibrahim705-dev <mohamedibrahim5577@gmail.com>
2026-09-14 12:13:59 +0300

    search view

--- real_estate/models/tenant.py
+++ real_estate/models/tenant.py
@@ -17,7 +17,26 @@ class Tenant(models.Model):
     notes = fields.Text(string='Notes')
     active = fields.Boolean(string='Active', default=True) 
     crm_lead_id = fields.Many2one('crm.lead', string='CRM Lead', ondelete='set null', index=True)
+    website = fields.Char(string='Website')
+    age_category = fields.Selection([
+        ('A', '1-20'),
+        ('B', '21-40'),
+        ('C', '41-60')],string='Age Category')
 
     def update_notes(self):
         for record in self:
             record.write({'notes' : record.name})
+
+    def update_Crm_Lead_Notes(self):
+            for record in self:
+                record.write({'notes' : record.crm_lead_id.name})
+    def update_Crm_Lead_website(self):
+            for record in self:
+                record.write({'notes' : record.crm_lead_id.website}) 
+    def crm_change_to_email(self):
+                for record in self:
+                    if record.crm_lead_id.website:
+                        record.write({'notes' : record.crm_lead_id.email_from})
+                    else:
+                        record.write({'notes' : record.website}) 
+                             
